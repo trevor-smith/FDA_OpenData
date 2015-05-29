253a5ec9b6c5897ec6b138a6b6fb295df4035367
trevor smith <trevor.stuard@gmail.com>
2015-05-29 16:07:52 -0400

    updated analysis

--- fda_data_entry.py
+++ fda_data_entry.py
@@ -9,19 +9,35 @@ import pprint
 from pymongo import MongoClient
 import time
 
-# already have some data in the db, now just querying it
-client = MongoClient()
-labels = client.drugs.drug_labeling
+#### EXTRACTING DRUG LABELS DATA ###
+# client = MongoClient()
+# labels = client.drugs.drug_labeling
 
 
-for i in range(0,1000):
-    j = int(i*100)
-    response = requests.get("https://api.fda.gov/drug/label.json?api_key=GETYOUROWNAPIKEY&search=&limit=100&skip=" + str(j) + "\"")
-    labels.insert(response.json())
+# for i in range(0,1000):
+#     j = int(i*100)
+#     if i % 240 == 0:
+#         time.sleep(30)
+#         print "finished iteration: " + str(i)
+#     response = requests.get("https://api.fda.gov/drug/label.json?api_key=h84pEssgNdT4A19c3CiHp1cKK3Gh3Aajc6GNtIq9&search=&limit=100&skip=" + str(i) + "\"")
+#     labels.insert(response.json())
     # loop to not run over the 240 requests per minute threshold
-    if i % 240 == 0:
-        time.sleep(30)
-        print "finished iteration: " + str(i)
 
+# cleaning up the bad data in mongodb
 # labels.remove( {'error': {u'message': u'Invalid skip parameter value.', u'code': u'BAD_REQUEST'}} )
 # labels.remove( {'error': {u'message': u'No matches found!', u'code': u'NOT_FOUND'}} )
+
+### EXTRACTING ADVERSE EVENTS DATA ###
+# there's over 4mm events from 2004
+
+client = MongoClient()
+events = client.drugs.adverse_events
+
+print "starting the adverse events extraction..."
+for i in range(0,45870):
+    j = int(i*100)
+    if i % 240 == 0:
+        time.sleep(59)
+        print "finished iteration: " + str(i)
+    response = requests.get("https://api.fda.gov/drug/event.json?api_key=h84pEssgNdT4A19c3CiHp1cKK3Gh3Aajc6GNtIq9&search=&limit=100&skip=" + str(i) + "\"")
+    events.insert(response.json())
---
+++ fda_initial_data_analysis.py
@@ -0,0 +1,110 @@
+##### THIS FILE IS FOR DATA ANALYSIS #####
+
+### IMPORTS ###
+from pymongo import MongoClient
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+
+### SETTING UP ENVIRONMENT ###
+client = MongoClient()
+labels = client.drugs.drug_labeling
+
+### QUERYING THE DATA ###
+# this would find all data...I have not given it any filters
+cursor = labels.find({})
+
+# this is just pulling the first document out of the database
+# and then appending it to a list called 'documents'
+# 'documents' will be a list of dictionaries
+documents = []
+for i in cursor:
+    documents.append(i['results'])
+
+# the elements of the 'documents' list are in fact dictionaries
+# need to split them
+data = []
+for i in documents:
+    for j in i:
+        data.append(j)
+
+### BASIC DATA ANALYSIS ###
+df = pd.DataFrame(data)
+
+# making our data strings because it is originally stored as a list
+df.warnings = df.warnings.astype(str)
+
+# # constructing tfidf matrix for each of our warning labels
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# vectorizer = TfidfVectorizer(min_df=1, stop_words='english',ngram_range=(2,4))
+# matrix = vectorizer.fit_transform(df.warnings)
+
+
+# extracting length from warning label
+from textblob import TextBlob
+
+
+# extracting data from openfda dictionary
+def clean_openfda_column():
+    df['brand_name'] = df.openfda.apply(lambda x: x['brand_name'] if 'brand_name' in x else 'unknown')
+    df['generic_name'] = df.openfda.apply(lambda x: x['generic_name'] if 'generic_name' in x else 'unknown')
+    df['is_original_packager'] = df.openfda.apply(lambda x: x['is_original_packager'] if 'is_original_packager' in x else 'unknown')
+    df['manufacturer_name'] = df.openfda.apply(lambda x: x['manufacturer_name'] if 'manufacturer_name' in x else 'unknown')
+    df['nui'] = df.openfda.apply(lambda x: x['nui'] if 'nui' in x else 'unknown')
+    df['package_ndc'] = df.openfda.apply(lambda x: x['package_ndc'] if 'package_ndc' in x else 'unknown')
+    df['pharm_class_epc'] = df.openfda.apply(lambda x: x['pharm_class_epc'] if 'pharm_class_epc' in x else 'unknown')
+    df['pharm_class_moa'] = df.openfda.apply(lambda x: x['pharm_class_moa'] if 'pharm_class_moa' in x else 'unknown')
+    df['product_ndc'] = df.openfda.apply(lambda x: x['product_ndc'] if 'product_ndc' in x else 'unknown')
+    df['product_type'] = df.openfda.apply(lambda x: x['product_type'] if 'product_type' in x else 'unknown')
+    df['route'] = df.openfda.apply(lambda x: x['route'] if 'route' in x else 'unknown')
+    df['rxcui'] = df.openfda.apply(lambda x: x['rxcui'] if 'rxcui' in x else 'unknown')
+    df['spl_id'] = df.openfda.apply(lambda x: x['spl_id'] if 'spl_id' in x else 'unknown')
+    df['spl_set_id'] = df.openfda.apply(lambda x: x['spl_set_id'] if 'spl_set_id' in x else 'unknown')
+    df['substance_name'] = df.openfda.apply(lambda x: x['substance_name'] if 'substance_name' in x else 'unknown')
+    df['unii'] = df.openfda.apply(lambda x: x['unii'] if 'unii' in x else 'unknown')
+    df['upc'] = df.openfda.apply(lambda x: x['upc'] if 'upc' in x else 'unknown')
+    df.product_type = df.product_type.astype(str)
+    df.product_type = df.product_type.apply(lambda x: x.strip('['))
+    df.product_type = df.product_type.apply(lambda x: x.strip(']'))
+
+def textblob_features():
+    df['polarity'] = df.warnings.apply(lambda x: TextBlob(x).polarity)
+    df['subjectivity'] = df.warnings.apply(lambda x: TextBlob(x).subjectivity)
+    df['length_of_warning'] = df.warnings.apply(lambda x: len(TextBlob(x).words))
+
+clean_openfda_column()
+textblob_features()
+### NLTK STUFF ###
+# from __future__ import division
+# import nltk
+# from nltk import word_tokenize
+
+# df.warnings = df.warnings.apply(lambda x: x.lower())
+# df.tokens = df.warnings.apply(lambda x: word_tokenize(x))
+# porter = nltk.PorterStemmer()
+# df.stemmed = df.tokens.apply(lambda x: [porter.stem(t) for t in x])
+
+# from sklearn.feature_extraction.text import CountVectorizer
+# vectorizer = CountVectorizer(min_df=1, stop_words='english')
+# matrix = vectorizer.fit_transform(df.warnings)
+
+
+
+# # initial plotting
+# df.length_of_warning.hist(bins=50)
+# plt.show()
+
+### WORKING WITH ADVERSE EVENTS ###
+
+# getting data out of MongoDB
+events = client.drugs.adverse_events
+cursor = events.find({})
+
+documents = []
+for i in cursor:
+    documents.append(i['results'])
+
+data = []
+for i in documents:
+    for j in i:
+        data.append(j)
