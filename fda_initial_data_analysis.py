6137d6e477695ac0fce31b4765f113c986b7344f
trevor smith <trevor.stuard@gmail.com>
2015-06-01 09:02:36 -0400

    added clustering

--- fda_initial_data_analysis.py
+++ fda_initial_data_analysis.py
@@ -18,7 +18,7 @@ cursor = labels.find({})
 # and then appending it to a list called 'documents'
 # 'documents' will be a list of dictionaries
 documents = []
-for i in cursor:
+for i in cursor[:20]:
     documents.append(i['results'])
 
 # the elements of the 'documents' list are in fact dictionaries
@@ -80,42 +80,75 @@ clean_openfda_column()
 textblob_features()
 
 ### NLTK STUFF ###
-from __future__ import division
-import nltk
-from nltk import word_tokenize
+# from __future__ import division
+# import nltk
+# from nltk import word_tokenize
 
-df.warnings = df.warnings.apply(lambda x: x.lower())
-df.tokens = df.warnings.apply(lambda x: word_tokenize(x))
-porter = nltk.PorterStemmer()
-df.stemmed = df.tokens.apply(lambda x: [porter.stem(t) for t in x])
+# df.warnings = df.warnings.apply(lambda x: x.lower())
+# df.tokens = df.warnings.apply(lambda x: word_tokenize(x))
+# porter = nltk.PorterStemmer()
+# df.stemmed = df.tokens.apply(lambda x: [porter.stem(t) for t in x])
 
-from sklearn.feature_extraction.text import CountVectorizer
-vectorizer = CountVectorizer(min_df=1, stop_words='english')
-matrix = vectorizer.fit_transform(df.warnings)
+# from sklearn.feature_extraction.text import CountVectorizer
+# vectorizer = CountVectorizer(min_df=1, stop_words='english')
+# matrix = vectorizer.fit_transform(df.warnings)
 
 
 
-# initial plotting
-df.length_of_warning.hist(bins=50)
-plt.show()
+# # initial plotting
+# df.length_of_warning.hist(bins=50)
+# plt.show()
 
 ### WORKING WITH ADVERSE EVENTS ###
 
 # getting data out of MongoDB
-events = client.drugs.adverse_events
-cursor = events.find({})
+# events = client.drugs.adverse_events
+# cursor = events.find({})
 
-documents = []
-for i in cursor:
-    documents.append(i['results'])
+# documents = []
+# for i in cursor:
+#     documents.append(i['results'])
 
-data = []
-for i in documents:
-    for j in i:
-        data.append(j)
+# data = []
+# for i in documents:
+#     for j in i:
+#         data.append(j)
 
 # want to take a subset so I can start clustering
 df_subset1 = df[['product_type', 'length_of_warning', 'polarity', 'subjectivity']]
 print df_subset1.head(3)
 
+df_subset1.drop('product_type', axis=1, inplace=True)
 
+### CLUSTERING ###
+from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
+from sklearn import metrics
+from sklearn.preprocessing import StandardScaler
+
+# preprocessing our data
+# standard scalar...converts data into z scores
+# this helps the clustering algorithms fit the data better
+
+X = StandardScaler().fit_transform(df_subset1)
+
+# ok, now to cluster with kmeans
+# choosing 3 clusters first because the FDA gives 3 example tones (mild, moderate, severe)
+num_clusters=3
+km = KMeans(init='random', max_iter=100, n_init=1, verbose=1, n_jobs=-1, n_clusters=num_clusters)
+km.fit(X)
+
+# now to use dbscan
+db = DBSCAN(eps=0.3, min_samples=10).fit(X)
+labels = db.labels_
+
+# Number of clusters in labels, ignoring noise if present.
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+
+print('Estimated number of clusters: %d' % n_clusters_)
+
+# ok, this says 8 clusters...let's re-fit kmeans
+
+num_clusters=8
+km = KMeans(init='random', max_iter=100, n_init=1, verbose=1, n_jobs=-1, n_clusters=num_clusters)
+km.fit(X)
