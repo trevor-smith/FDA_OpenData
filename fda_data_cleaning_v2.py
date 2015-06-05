1c684684a5b34b2d30f3daa831fe3e1237cb8d10
trevor smith <trevor.stuard@gmail.com>
2015-06-05 16:32:56 -0400

    finalized flattening function

--- fda_data_cleaning_v2.py
+++ fda_data_cleaning_v2.py
@@ -14,80 +14,92 @@ client = MongoClient()
 labels = client.drugs.drug_labeling
 events = client.drugs.adverse_events
 
-### QUERYING THE DATA ###
-# this would find all data...I have not given it any filters
-cursor_labels = labels.find({})
-cursor_events = events.find({})
+### STEP 1: FLATTEN THE DATA ###
 
-# this is just pulling the mongodb documents out of the database
-# and then appending it to a list called 'documents'
-# 'documents' will be a list of dictionaries
-documents_labels = []
-for i in cursor_labels:
-    documents_labels.append(i['results'])
-
-documents_events = []
-for i in cursor_events:
-    documents_events.append(i['results'])
-
-# the elements of the 'documents' list are in fact dictionaries
-# need to split them
-data_labels = []
-for i in documents_labels:
-    for j in i:
-        data_labels.append(j)
-
-data_events = []
-for i in documents_events:
-    for j in i:
-        data_events.append(j)
+""" the data is nested JSON and I'd like to get it tabular so I'm going
+to flatten it with the function I've created below.  This one is extracting
+the data related to the patient and the drugs they have in their system. """
 
 
-### CLEANING THE DATA ###
+def flatten_patients_data():
 
-### FLATTENING DATA ###
-# data already exists in list called 'data_events'
+    """ this function reads in the data from mongodb in chunks because
+    the size of the data is too large to fit into memory.  I read in each
+    iteration and then extract the data I need and save to csv"""
 
-print len(data_events)
-# this is 223,200
+    """ this is how we are going to iterate through the mongodb cursor """
 
-test = []
-headers = ['safetyreportid', 'receivedate', 'country', 'serious', 'transmissiondate', 'actiondrug', 'medicinalproduct', 'drugcharacterization']
-# the list of our keys that aren't nested
-first = ['safetyreportid', 'receivedate', 'occurcountry', 'serious', 'transmissiondate']
-# the list of the keys that are nested
-second = ['actiondrug', 'medicinalproduct', 'drugcharacterization']
-keys = ['medicinalproduct', 'actiondrug', 'drugcharacterization']
-test.append(headers)
+    print "here we go............."
+    for i in range(14):
+        tracker = i
 
-# the goal is to create a list of lists
-# sometimes a key does not exist...if it doesn't, then we skip that row
-for i in data_events:
-    for j in i['patient']['drug']:
-        if all (k in j for k in keys):
-            row = []
-            # this loop appends the items that are not as far nested
-            # that's why it refers to i
-            for m in first:
-                row.append(i[m])
-            # this loop appends the items that are more nested
-            # that's why it refers to j
-            for n in second:
-                row.append(j[n])
-            test.append(row)
-        else:
-            pass
+        """ num and num_1000 are how I am going to iterate through the
+        cursor by taking slices """
 
-print len(test)
-# now it has been expanded to 351,097
+        num = i*1000
+        num_1000 = num + 1000
+        cursor_events = events.find({})[num:num_1000]
+        documents_events = []
+        for j in cursor_events:
+            documents_events.append(j['results'])
+        print "done with documents step: " + str(tracker)
 
-### SAVING TO CSV ###
-# we want to save this file as a csv
-# we can then store it in sql OR just read it right into a pandas DF
+        """ there are 100 items stored in each document so now I have to
+        iterate through each document and pull out the specific item.  One
+        document contains 100 items... """
 
-import csv
+        data_events = []
+        for k in documents_events:
+            for l in k:
+                data_events.append(l)
+        print "done with data step: " + str(tracker)
 
-with open("patients.csv", "wb") as f:
-    writer = csv.writer(f)
-    writer.writerows(test)
+        """ now we have to start to flatten out the data.  The data is
+        nested JSON so I'm only going to extract the parts that I need.
+        There are two levels of keys that I'll be extracting from...
+        first and second """
 
+        # test is the list of lists we are going to append to
+        test = []
+
+        # headers are the headers of the list of lists file
+        headers = ['safetyreportid', 'receivedate', 'country', 'serious', 'transmissiondate', 'actiondrug', 'medicinalproduct', 'drugcharacterization']
+
+        # the list of our keys that aren't nested
+        first = ['safetyreportid', 'receivedate', 'occurcountry', 'serious', 'transmissiondate']
+
+        # the list of the keys that are nested
+        second = ['actiondrug', 'medicinalproduct', 'drugcharacterization']
+        keys = ['medicinalproduct', 'actiondrug', 'drugcharacterization']
+        test.append(headers)
+
+        """ the goal is to create a list of lists. sometimes a key does
+        not exist...if it doesn't, then we skip that row """
+
+        for i in data_events:
+            for j in i['patient']['drug']:
+                if all (k in j for k in keys):
+                    row = []
+                    # this loop appends the items that are not as far nested
+                    # that's why it refers to i
+                    for m in first:
+                        row.append(i[m])
+                    # this loop appends the items that are more nested
+                    # that's why it refers to j
+                    for n in second:
+                        row.append(j[n])
+                    test.append(row)
+                else:
+                    pass
+
+        """ now that we have our data flattened out into list of lists
+        it is time to save it to a csv file """
+
+        import csv
+
+        with open("patients"+"_"+str(tracker)+".csv", "wb") as f:
+            writer = csv.writer(f)
+            writer.writerows(test)
+        print "done with csv step: " + str(tracker)
+
+flatten_patients_data()
